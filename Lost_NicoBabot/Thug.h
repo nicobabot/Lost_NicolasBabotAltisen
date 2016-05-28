28f3464be3834e6c54b4f2f8c05f626897b13237
nicobabot <nico.babotaltisen@gmail.com>
2016-05-28 10:19:59 +0200

    Update Error thug

--- Lost_NicoBabot/Thug.h
+++ Lost_NicoBabot/Thug.h
@@ -3,12 +3,18 @@
 #include"Room.h"
 #include"Creature.h"
 #include"Entity.h"
+#include<time.h>
 class Thug : public Creature{
 public:
 	Thug(const char* name, const char* descrip, const int health, const int damage, const int accuracy, Room* pos) : Creature(name, descrip, health, damage, accuracy, THUG, pos){
 
 	}
-	void Thug::Update(){
+	void Update(){
+		int i = (rand() % 3) + 4;
+		world->thug->position = (Room*)world->entities[i];
+		if (world->player->position == world->thug->position){
+			printf("HI I AM A THUG");
+		}
 		//printf("HI\n");
 	}
 };
